9f2f41b3e6d0ef4ba601d0f821baf52058941991
Grace kungu <grayckungu2@gmail.com>
2023-06-15 08:45:07 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(){
+    return 2000/100;
+}
+console.log(divide());// Output:20..
+
+const square =(Num)=>  Num*Num;{
+ console.log(square(2));
+}
+square(2);//=> 4
+
+
+const add = (a,b) =>  a+b;{
+let a = 3
+let b = 4
+console.log(add(3,4));//=> 7
+}
